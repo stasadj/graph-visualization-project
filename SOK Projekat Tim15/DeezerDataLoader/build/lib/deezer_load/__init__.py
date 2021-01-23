fe64eb9e278cec193bf507031d38d8b7260fb1e7
Dina Petrov <dina.petrov@gmail.com>
2021-01-23 17:54:24 +0100

    Obrisani build, dist i egg fajlovi (Issue #5)

--- SOK Projekat Tim15/DeezerDataLoader/build/lib/deezer_load/__init__.py
+++
@@ -1,6 +0,0 @@
-# Potrebno zbog namespace_pakages
-# parametra u setup.py modulu
-# Ovaj deo koda je potreban da bi paket bio prijavljen
-# kao namespace.
-
-__import__('pkg_resources').declare_namespace(__name__)
